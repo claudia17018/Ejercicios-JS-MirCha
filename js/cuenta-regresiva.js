ab04b5d9501670a051c32c4945e1bb2b59d32f69
Rox <rox98lopez@gmail.com>
2021-07-17 09:04:59 -0600

    Cuenta Regresiva

---
+++ js/cuenta-regresiva.js
@@ -0,0 +1,17 @@
+const d= document;
+
+export function cuentaRegresiva(id,fin,mensaje){
+ const $countdown=d.getElementById(id),countdownDate=new Date(fin).getTime();
+
+ let $countdownTempo=setInterval(()=>{
+     let now =new Date().getTime(),limitTime=countdownDate-now,
+     days =Math.floor(limitTime/(1000*60*60*24)),
+     hours=("0"+ Math.floor((limitTime%(1000*60*60*24))/(1000*60*60))).slice(-2),
+     minutes=("0"+ Math.floor((limitTime%(1000*60*60))/(1000*60))).slice(-2),
+     seconds=("0"+ Math.floor((limitTime%(1000*60))/1000)).slice(-2);
+    $countdown.innerHTML=`<h3>Faltan ${days} dias ${hours} horas ${minutes} minutos ${seconds} segundos </h3>`;
+ if(limitTime<0){
+     clearInterval(countdownDate)
+ }
+ }, 1000);
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,3 +1,4 @@
+import { cuentaRegresiva } from "./cuenta-regresiva.js";
 import hamburgerMenu from "./menu.js";
 import { digitalClock, alarm } from "./reloj.js";
 import {moveBall,shortcuts} from "./teclado.js";
@@ -8,6 +9,7 @@ d.addEventListener("DOMContentLoaded", (e)=> {
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
     alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
+    cuentaRegresiva("countdown","May 23,2022","Feliz Dia");
 });
 
 d.addEventListener("keydown", (e)=>{
