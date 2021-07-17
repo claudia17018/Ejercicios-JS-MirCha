3ac83b832587ae798fe439c16c1c71bc92966105
Rox <rox98lopez@gmail.com>
2021-07-16 18:38:23 -0600

    Reloj Digital

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,7 +1,9 @@
 import hamburgerMenu from "./menu.js";
+import { digitalClock } from "./reloj.js";
 
 const d=document;
 
 d.addEventListener("DOMContentLoaded", (e)=> {
     hamburgerMenu(".panel-btn",".panel",".menu a");
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
 });
