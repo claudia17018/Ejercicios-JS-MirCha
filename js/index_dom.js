32146dcf27e8f20a1ed406da4a115215dc162245
Rox <rox98lopez@gmail.com>
2021-07-17 11:05:58 -0600

    Modo oscuro

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,7 @@
 import { scrollTopBoton } from "./boton_scroll.js";
 import { cuentaRegresiva } from "./cuenta-regresiva.js";
 import hamburgerMenu from "./menu.js";
+import darkTheme from "./oscuro.js";
 import { digitalClock, alarm } from "./reloj.js";
 import {moveBall,shortcuts} from "./teclado.js";
 
@@ -12,6 +13,7 @@ d.addEventListener("DOMContentLoaded", (e)=> {
     alarm("assets/alarma.mp3","#activar-alarma","#desactivar-alarma");
     cuentaRegresiva("countdown","May 23,2022","Feliz Dia");
     scrollTopBoton(".scroll-top-btn");
+    darkTheme(".dark-theme-btn","dark-mode");
 });
 
 d.addEventListener("keydown", (e)=>{
